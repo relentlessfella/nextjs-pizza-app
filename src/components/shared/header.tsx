756990d9aed02dcbf2749398c9861083e373e565
Nurgali <nrknrps@gmail.com>
2024-09-06 15:29:41 +0500

    initial state

---
+++ src/components/shared/header.tsx
@@ -0,0 +1,46 @@
+import { cn } from '@/lib/utils';
+import React from 'react';
+import { Container } from './container';
+import Image from 'next/image';
+import { Button } from '../ui/button';
+import { ShoppingCart, User } from 'lucide-react';
+
+interface Props {
+  className?: string;
+}
+
+export const Header: React.FC<Props> = ({ className }) => {
+  return (
+    <header className={cn('border border-b', className)}>
+      <Container className="flex items-center justify-between py-8">
+        {/* Левая часть */}
+        <div className="flex items-center gap-4">
+          <Image src="/newlogo.jpeg" alt="Logo" width={35} height={35} className="rounded" />
+          <div className="">
+            <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
+            <p className="text-sm text-gray-400 leading-3">вкусней уже некуда</p>
+          </div>
+        </div>
+
+        {/* Правая часть */}
+        <div className="flex items-center gap-3">
+          <Button variant="outline" className="flex items-centers gap-1">
+            <User size={16} />
+            Войти
+          </Button>
+
+          <div>
+            <Button className="group relative">
+              <b>520 $</b>
+              <span className="h-full w-[1px] bg-white/30 mx-3"></span>
+              <div className="flex items-center gap-1 transition duration-300 group-hover:opacity-0">
+                <ShoppingCart className="h-4 w-4 relative" strokeWidth={2}></ShoppingCart>
+                <b>3</b>
+              </div>
+            </Button>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
+};
