e2ae6526107eb4ca5df44a032414e6c5c1f89f3a
Nurgali <nrknrps@gmail.com>
2024-09-06 16:58:21 +0500

    topbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 import Image from 'next/image';
 import { Button } from '@/components/ui/button';
 import { Popover, PopoverAnchor, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { Container } from '@/components/shared/container';
+import { Title } from '@/components/shared/title';
+import { Categories } from '@/components/shared/categories';
+import { SortPopup } from '@/components/shared/sort-popup';
+import { TopBar } from '@/components/shared/top-bar';
 
 export default function Home() {
   return (
-    <h1 className="flex justify-center">
-      <Button variant="outline">Button</Button>
-    </h1>
+    <>
+      <Container className="mt-10">
+        <Title text="Все пиццы" size="lg" className="font-extrabold"></Title>
+      </Container>
+      <TopBar />
+    </>
   );
 }
